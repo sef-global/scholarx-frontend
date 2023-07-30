74317ab799427b07c22352f5435db0fa03cf835d
Methma Peiris <61134846+MethmaPeiris99@users.noreply.github.com>
2023-07-30 16:57:43 +0530

    Change the code to use map function to display the questions and answers

--- src/components/Home/FAQ/MenteeFAQCollapse.tsx
+++ src/components/Home/FAQ/MenteeFAQCollapse.tsx
@@ -1,45 +1,27 @@
 import React from 'react'
 import { Collapse, Typography } from 'antd';
 import styles from './FAQCollapse.css'
-import {menteeFAQs} from "./FAQs";
+import menteeFAQs from "./MenteeFAQs.json";
 
 const { Panel } = Collapse;
-const {Text} = Typography;
+const {Paragraph} = Typography;
 
 function MenteeFAQCollapse() {
 
     return(
         <Collapse className={styles.faqCollapseContainer}>
-            <Panel key={1} header="What are the eligibility requirements?">
-                <Text className={styles.antTypography}>
-                    Our priority is given to applicants of Sri Lankan origin. However, if we have an excess of mentors,
-                    will look at expanding the offering to foreign students as well.
-                </Text>
-                <ul className={styles.faqAnswer}>
-                    <li>You must be at least 18 years of age when you register</li>
-                    <li>You must currently be a full time or part-time student undertaking an undergraduate or postgraduate course</li>
-                    <li>You have not already been accepted as a mentee in ScholarX more than once</li>
-                </ul>
-            </Panel>
-            <Panel key={2} header= "What are the selection criteria?">
-                <Text className={styles.antTypography}>
-                    The application form consists of a series of questions regarding your field of study, extracurricular experience
-                    and interest in the program, designed to allow for the best possible match between mentors and mentees. The application
-                    process is competitive so we encourage students to be as thorough as possible in answering the short answer section of the
-                    application form.
-                </Text>
-                <Text className={styles.antTypography}>
-                    We require all applicants to demonstrate a high standard of English proficiency and a genuine curiosity to learn and innovate.
-                    In their responses, applicants must also show ambition to achieve their career goals and articulately express how the mentoring
-                    program will assist with achieving their overall academic and/or professional objectives.
-                </Text>
-                <Text className={styles.antTypography}>
-                    Once selections and assignments are made, mentees will be contacted with further information on how to proceed.
-                </Text>
-            </Panel>
-            {menteeFAQs.map((item, index) => (
-                <Panel key={index} header={item.question}>
-                    <Text className={styles.antTypography}>{item.answer}</Text>
+            {menteeFAQs.map((menteeFAQ, index) => (
+                <Panel key={index} header={menteeFAQ.question}>
+                    {menteeFAQ.answer.paragraph.map((paragraphItem, paragraphIndex) => (
+                        <Paragraph key={paragraphIndex} className={styles.antTypography}>{paragraphItem}</Paragraph>
+                    ))}
+                    {menteeFAQ.answer.list.length > 0 && (
+                        <ul className={styles.faqAnswer}>
+                            {menteeFAQ.answer.list.map((listItem, listIndex) => (
+                                <li key={listIndex}>{listItem}</li>
+                            ))}
+                        </ul>
+                    )}
                 </Panel>
             ))}
         </Collapse>
--- src/components/Home/FAQ/MentorFAQCollapse.tsx
+++ src/components/Home/FAQ/MentorFAQCollapse.tsx
@@ -1,38 +1,29 @@
 import React from 'react'
 import { Collapse, Typography } from 'antd';
 import styles from './FAQCollapse.css'
-import {mentorFAQs} from "./FAQs";
+import mentorFAQs from "./MentorFAQs.json";
 
 const { Panel } = Collapse;
-const {Text} = Typography;
+const {Paragraph} = Typography;
 
 function MentorFAQCollapse() {
 
     return(
         <Collapse className={styles.faqCollapseContainer}>
-            <Panel key={1} header={mentorFAQs[0].question}>
-                <Text className={styles.antTypography}>{mentorFAQs[0].answer}</Text>
-            </Panel>
-            <Panel key={2} header="How do I apply to be a mentor?">
-                <Text className={styles.antTypography}>
-                    Applications for the mentors of the ScholarX program 2023 will open on
-                    13th of March 2023 and will close 11th of April 2023. Since the application period is over,
-                    you may submit your CV, a headshot, and expression of interest to
-                    <a href="mailto: sustainableedufoundation@gmail.com"> sustainableedufoundation@gmail.com</a>
-                </Text>
-            </Panel>
-            <Panel key={3} header={mentorFAQs[1].question}>
-                <Text className={styles.antTypography}>{mentorFAQs[1].answer}</Text>
-            </Panel>
-            <Panel key={4} header={mentorFAQs[2].question}>
-                <Text className={styles.antTypography}>{mentorFAQs[2].answer}</Text>
-            </Panel>
-            <Panel key={5} header={mentorFAQs[3].question}>
-                <Text className={styles.antTypography}>{mentorFAQs[3].answer}</Text>
-            </Panel>
-            <Panel key={6} header={mentorFAQs[4].question}>
-                <Text className={styles.antTypography}>{mentorFAQs[4].answer}</Text>
-            </Panel>
+            {mentorFAQs.map((mentorFAQ, index) => (
+                <Panel key={index} header={mentorFAQ.question}>
+                    {mentorFAQ.answer.paragraph.map((paragraphItem, paragraphIndex) => (
+                      <Paragraph key={paragraphIndex} className={styles.antTypography}>{paragraphItem}</Paragraph>
+                    ))}
+                    {mentorFAQ.answer.list.length > 0 && (
+                        <ul className={styles.faqAnswer}>
+                            {mentorFAQ.answer.list.map((listItem, listIndex) => (
+                                <li key={listIndex}>{listItem}</li>
+                            ))}
+                        </ul>
+                    )}
+                </Panel>
+            ))}
         </Collapse>
     )
 }
