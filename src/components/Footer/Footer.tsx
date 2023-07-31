ef0d0d146b5d09c43684fb21e4076f8bbb7169e5
Methma Peiris <61134846+MethmaPeiris99@users.noreply.github.com>
2023-07-31 16:03:26 +0530

    Initialize NavBar and Footer components

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Col, Row } from 'antd';
+
+const Footer: React.FC = () => (
+  <Row>
+    <Col>
+      <h1>footer</h1>
+    </Col>
+  </Row>
+);
+export default Footer;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Col, Row } from 'antd';
+
+const NavBar: React.FC = () => (
+  <Row>
+    <Col>
+      <h1>nav bar</h1>
+    </Col>
+  </Row>
+);
+export default NavBar;
