d931a9955edc79c1033e685e14ed2c2bc66baaf3
Methma Peiris <61134846+MethmaPeiris99@users.noreply.github.com>
2023-07-31 16:18:48 +0530

    Add Layout in ant design

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
+import { Layout } from 'antd';
 import ReactDOM from 'react-dom';
 
+import FooterComponent from './components/Footer/FooterComponent';
 import Home from './components/Home/Home';
+import NavBar from './components/NavBar/NavBar';
 
-function App() {
-  return (
-    <>
+const { Header, Footer, Content } = Layout;
+
+const App: React.FC = () => (
+  <Layout>
+    <Header>
+      <NavBar />
+    </Header>
+    <Content>
       <Home />
-    </>
-  );
-}
-
+    </Content>
+    <Footer>
+      <FooterComponent />
+    </Footer>
+  </Layout>
+);
 ReactDOM.render(<App />, document.getElementById('root'));
