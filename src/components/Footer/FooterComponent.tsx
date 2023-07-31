cb98f7afebeb85a96b2692a5fe26e906a3a7cc3b
Methma Peiris <61134846+MethmaPeiris99@users.noreply.github.com>
2023-07-31 16:18:19 +0530

    Change the Footer name to FooterComponent

--- src/components/Footer/FooterComponent.tsx
+++ src/components/Footer/FooterComponent.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import { Col, Row } from 'antd';
 
-const Footer: React.FC = () => (
+const FooterComponent: React.FC = () => (
   <Row>
     <Col>
       <h1>footer</h1>
     </Col>
   </Row>
 );
-export default Footer;
+export default FooterComponent;
