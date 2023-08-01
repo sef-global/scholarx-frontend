e87d858e9e9adeff046f0d5ee4279b73841c874d
Methma Peiris <61134846+MethmaPeiris99@users.noreply.github.com>
2023-08-01 15:05:40 +0530

    Add elements for navbar and footer

--- src/components/Footer/FooterComponent.tsx
+++ src/components/Footer/FooterComponent.tsx
@@ -1,11 +1,93 @@
 import React from 'react';
 
-import { Col, Row } from 'antd';
+import {
+  FacebookFilled,
+  GithubOutlined,
+  InstagramFilled,
+  LinkedinFilled,
+  SlackOutlined,
+  TwitterOutlined,
+  YoutubeFilled,
+} from '@ant-design/icons';
+import { Button, Col, Row, Space, Typography } from 'antd';
+
+import styles from './FooterComponent.css';
+
+const { Text } = Typography;
 
 const FooterComponent: React.FC = () => (
   <Row>
-    <Col>
-      <h1>footer</h1>
+    <Col span={8}>
+      <Space direction="vertical">
+        <Text className={styles.footerMainTitle}>Let`s Rethink Education!</Text>
+        <Text className={styles.footerSecondaryTitle}>
+          Get in touch with using any of these platforms.
+        </Text>
+        <Space direction="horizontal" size={10}>
+          <Button
+            className={styles.antButton}
+            style={{ backgroundColor: '#1DA1F2' }}
+            icon={<TwitterOutlined className={styles.antIcon} />}
+          />
+          <Button
+            className={styles.antButton}
+            style={{ backgroundColor: '#3B5999' }}
+            icon={<FacebookFilled className={styles.antIcon} />}
+          />
+          <Button
+            className={styles.antButton}
+            style={{ backgroundColor: '#0077B5' }}
+            icon={<LinkedinFilled className={styles.antIcon} />}
+          />
+          <Button
+            className={styles.antButton}
+            style={{ backgroundColor: '#E4405F' }}
+            icon={<InstagramFilled className={styles.antIcon} />}
+          />
+          <Button
+            className={styles.antButton}
+            style={{ backgroundColor: '#222222' }}
+            icon={<GithubOutlined className={styles.antIcon} />}
+          />
+          <Button
+            className={styles.antButton}
+            style={{ backgroundColor: '#CD201F' }}
+            icon={<YoutubeFilled className={styles.antIcon} />}
+          />
+          <Button
+            className={styles.antButton}
+            type="primary"
+            style={{ backgroundColor: '#3AAF85' }}
+            icon={<SlackOutlined className={styles.antIcon} />}
+          />
+          <Button
+            className={styles.antButton}
+            style={{ backgroundColor: '#5E72E4' }}
+            icon={
+              <img src="/discourse-icon.png" alt="discourse-icon" width={15} />
+            }
+          />
+        </Space>
+      </Space>
+    </Col>
+    <Col span={8} offset={8}>
+      <Space direction="horizontal" size={25}>
+        <Text className={styles.antTypography}>Home</Text>
+        <Text className={styles.antTypography}>Projects</Text>
+        <Text className={styles.antTypography}>Team</Text>
+        <Text className={styles.antTypography}>Blog</Text>
+        <Text className={styles.antTypography}>Join Us</Text>
+        <Text className={styles.antTypography}>Handbook</Text>
+      </Space>
+    </Col>
+    <Col span={24} style={{ textAlign: 'center' }}>
+      <Text className={styles.footerLink}>
+        © 2023
+        <a href="https://sefglobal.org/" className={styles.sefLink}>
+          {' '}
+          Sustainable Education Foundation - SEF.
+        </a>
+      </Text>
     </Col>
   </Row>
 );
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 
-import { Col, Row } from 'antd';
+import {
+  FacebookFilled,
+  TwitterOutlined,
+  LinkedinFilled,
+  InstagramOutlined,
+} from '@ant-design/icons';
+import { Button, Col, Row, Space, Typography } from 'antd';
+
+import styles from './NavBar.css';
+
+const { Text } = Typography;
 
 const NavBar: React.FC = () => (
-  <Row>
-    <Col>
-      <h1>nav bar</h1>
+  <Row align={'middle'}>
+    <Col span={3}>
+      <img className={styles.navbarLogoContainer} src="./scholarx-logo.png" />
+    </Col>
+    <Col span={12}>
+      <Space size={80}>
+        <Text className={styles.antTypography}>Home</Text>
+        <Text className={styles.antTypography}>About</Text>
+        <Text className={styles.antTypography}>Initiatives</Text>
+        <Text className={styles.antTypography}>Join Us</Text>
+      </Space>
+    </Col>
+    <Col span={4} offset={5}>
+      <Space size={10}>
+        <Button className={styles.antButton} shape="circle">
+          <img src="/facebook-icon.png" width={20} />
+        </Button>
+        <Button className={styles.antButton} shape="circle">
+          <TwitterOutlined className={styles.antIcon} />
+        </Button>
+        <Button className={styles.antButton} shape="circle">
+          <LinkedinFilled className={styles.antIcon} />
+        </Button>
+        <Button className={styles.antButton} shape="circle">
+          <InstagramOutlined className={styles.antIcon} />
+        </Button>
+        <Button>Join Us</Button>
+      </Space>
     </Col>
   </Row>
 );
