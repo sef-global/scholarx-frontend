30cdbf17a83b3702f38b3ec07aa05b4f354dae6e
Methma Peiris <61134846+MethmaPeiris99@users.noreply.github.com>
2023-08-06 12:14:00 +0530

    Integrate more responsiveness

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,74 +1,127 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
+  AlignRightOutlined,
   TwitterOutlined,
   LinkedinFilled,
   InstagramOutlined,
 } from '@ant-design/icons';
-import { Button, Col, Row, Space, Typography } from 'antd';
+import { Button, Col, Drawer, Row, Space, Typography } from 'antd';
 
 import styles from './NavBar.css';
 
 const { Text } = Typography;
 
-const NavBar: React.FC = () => (
-  <Row align={'middle'}>
-    <Col span={15}>
-      <Space size={80} direction="horizontal">
-        <img className={styles.navbarLogoContainer} src="./scholarx-logo.png" />
-        <a href="https://sefglobal.org/">
-          <Text className={styles.antTypography}>Home</Text>
-        </a>
-        <a href="" target="_blank" rel="noreferrer">
-          <Text className={styles.antTypography}>About</Text>
-        </a>
-        <a href="" target="_blank" rel="noreferrer">
-          <Text className={styles.antTypography}>Initiatives</Text>
-        </a>
-        <a href="" target="_blank" rel="noreferrer">
-          <Text className={styles.antTypography}>Join Us</Text>
-        </a>
-      </Space>
-    </Col>
-    <Col span={4} offset={5}>
-      <Space size={10} direction="horizontal">
-        <a
-          href="https://www.facebook.com/sustainableeducationfoundation/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Button
-            className={styles.antButton}
-            shape="circle"
-            icon={<img src="/facebook-icon.png" width={20} />}
+const NavBar: React.FC = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return (
+    <>
+      <Row align={'middle'} justify={'start'}>
+        <Col xs={20} md={2} lg={4} xl={4} xxl={4}>
+          <img
+            className={styles.navbarLogoContainer}
+            src="./scholarx-logo.png"
           />
-        </a>
-        <a href="https://twitter.com/goasksef" target="_blank" rel="noreferrer">
-          <Button className={styles.antButton} shape="circle">
-            <TwitterOutlined className={styles.antIcon} />
-          </Button>
-        </a>
-        <a
-          href="https://www.linkedin.com/company/sefglobal/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Button className={styles.antButton} shape="circle">
-            <LinkedinFilled className={styles.antIcon} />
-          </Button>
-        </a>
-        <a
-          href="https://www.instagram.com/sefglobal/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Button className={styles.antButton} shape="circle">
-            <InstagramOutlined className={styles.antIcon} />
-          </Button>
-        </a>
-        <Button className={styles.loginButton}>Join Us</Button>
-      </Space>
-    </Col>
-  </Row>
-);
+        </Col>
+        <AlignRightOutlined
+          className={styles.menuIcon}
+          onClick={() => {
+            setOpenMenu(true);
+          }}
+        />
+        <Col md={16} lg={14} xl={15} xxl={16}>
+          <Space direction="horizontal">
+            <div className={styles.navbarItemContainer}>
+              <a href="https://sefglobal.org/">
+                <Text className={styles.antTypography}>Home</Text>
+              </a>
+              <a href="" target="_blank" rel="noreferrer">
+                <Text className={styles.antTypography}>About</Text>
+              </a>
+              <a href="" target="_blank" rel="noreferrer">
+                <Text className={styles.antTypography}>Initiatives</Text>
+              </a>
+              <a href="" target="_blank" rel="noreferrer">
+                <Text className={styles.antTypography}>Join Us</Text>
+              </a>
+            </div>
+          </Space>
+        </Col>
+        <Col md={4} lg={4} xl={3} className={styles.socialMediaContainer}>
+          <Space direction="horizontal">
+            <a
+              href="https://www.facebook.com/sustainableeducationfoundation/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button
+                className={styles.antButton}
+                shape="circle"
+                icon={<img src="/facebook-icon.png" width={20} />}
+              />
+            </a>
+            <a
+              href="https://twitter.com/goasksef"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button className={styles.antButton} shape="circle">
+                <TwitterOutlined className={styles.antIcon} />
+              </Button>
+            </a>
+            <a
+              href="https://www.linkedin.com/company/sefglobal/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button className={styles.antButton} shape="circle">
+                <LinkedinFilled className={styles.antIcon} />
+              </Button>
+            </a>
+            <a
+              href="https://www.instagram.com/sefglobal/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button className={styles.antButton} shape="circle">
+                <InstagramOutlined className={styles.antIcon} />
+              </Button>
+            </a>
+            <Button className={styles.loginButton}>Join Us</Button>
+          </Space>
+        </Col>
+      </Row>
+      <Drawer
+        open={openMenu}
+        placement="right"
+        onClose={() => {
+          setOpenMenu(false);
+        }}
+        title={
+          <img
+            src="./scholarx-logo.png"
+            className={styles.drawerLogoContainer}
+          />
+        }
+        closable
+      >
+        <Space direction="vertical">
+          <a href="https://sefglobal.org/">
+            <Text className={styles.antTypography}>Home</Text>
+          </a>
+          <a href="" target="_blank" rel="noreferrer">
+            <Text className={styles.antTypography}>About</Text>
+          </a>
+          <a href="" target="_blank" rel="noreferrer">
+            <Text className={styles.antTypography}>Initiatives</Text>
+          </a>
+          <a href="" target="_blank" rel="noreferrer">
+            <Text className={styles.antTypography}>Join Us</Text>
+          </a>
+        </Space>
+      </Drawer>
+    </>
+  );
+};
 export default NavBar;
