74c549ef588c5667ac68b45059e12471cacfbf5c
Methma Peiris <61134846+MethmaPeiris99@users.noreply.github.com>
2023-08-06 13:06:39 +0530

    Create a Layout component to render content

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 
-import { Layout } from 'antd';
 import ReactDOM from 'react-dom';
 
-import FooterComponent from './components/Footer/FooterComponent';
 import Home from './components/Home/Home';
-import NavBar from './components/NavBar/NavBar';
-import styles from './index.css';
-
-const { Header, Footer, Content } = Layout;
+import LayoutComponent from './components/Layout/LayoutComponent';
 
 const App: React.FC = () => (
-  <Layout>
-    <Header className={styles.antLayoutHeader}>
-      <NavBar />
-    </Header>
-    <Content className={styles.antLayoutContent}>
-      <Home />
-    </Content>
-    <Footer className={styles.antLayoutFooter}>
-      <FooterComponent />
-    </Footer>
-  </Layout>
+  <LayoutComponent>
+    <Home />
+  </LayoutComponent>
 );
 ReactDOM.render(<App />, document.getElementById('root'));
