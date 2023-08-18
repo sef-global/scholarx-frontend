442066ae99861c09ce619985c14115418e19b714
Randila Premarathne <randilamenukapremarathne@gmail.com>
2023-08-18 19:40:14 +0530

    Remove link

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Home from './components/Home/Home';
 import MainLayout from './components/Layout/MainLayout';
-import UserLogin from './components/UserLogin/UserLogin.component';
 
 const App: React.FC = () => (
   <MainLayout>
