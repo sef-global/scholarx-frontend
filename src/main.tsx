cc6a3951bb69c89d603c583bdd38d603779f3a1c
Madhawa Monarawila <70215958+Madhawa97@users.noreply.github.com>
2023-09-24 17:26:00 +0530

    Configure tailwind (#49)

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 
 root.render(
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./index.html', './src/**/*.{js,ts,jsx,tsx}'];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
