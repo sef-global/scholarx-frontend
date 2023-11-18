9a3eeeb80622a44cf8cbd9e190264f31f072ce5d
Kumuditha Karunarathna <kumudithaudaya@gmail.com>
2023-11-18 17:06:51 +0530

    Implement the login component design (#59)

--- src/components/LoginModal/index.tsx
+++ src/components/LoginModal/index.tsx
@@ -4,6 +4,7 @@ import { API_URL } from '../../constants';
 import { type Profile } from '../../types';
 import { UserContext, type UserContextType } from '../../contexts/UserContext';
 import closeIcon from '../../assets/svg/closeIcon.svg';
+import styles from './LoginModal.module.css';
 
 interface LoginModalProps {
   onClose: () => void;
@@ -69,86 +70,84 @@ const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
           </button>
 
           <div className="bg-white p-6 space-y-8 rounded-lg shadow-xl">
-            <h2 className="text-2xl font-bold text-gray-900 text-center">
-              Log in to ScholarX
-            </h2>
-            <form className="mt-8 space-y-6" onSubmit={handleLogin}>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                  placeholder="name@company.com"
-                  value={email}
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                  required
+            <div className={styles.modalWrapper}>
+              <h2 className="text-2xl font-bold text-gray-900 tracking-widest text-center">
+                Welcome back!
+              </h2>
+              <div className={styles.scholarxLogoWrapper}>
+                <img
+                  src="../../../public/scholarx-logo.png"
+                  alt="scholarx-logo"
                 />
               </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Your password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                  value={password}
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                  required
-                />
-              </div>
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
+              <form className="mt-8 space-y-6" onSubmit={handleLogin}>
+                <div>
                   <input
-                    id="remember"
-                    aria-describedby="remember"
-                    name="remember"
-                    type="checkbox"
-                    className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    required
                   />
                 </div>
-                <div className="ml-3 text-sm">
-                  <label
-                    htmlFor="remember"
-                    className="font-medium text-gray-500"
-                  >
-                    Remember this device
-                  </label>
+                <div>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    required
+                  />
                 </div>
-                <a
-                  href="#"
-                  className="ml-auto text-sm font-medium text-blue-600 hover:underline"
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      name="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      htmlFor="remember"
+                      className="font-normal text-gray-500"
+                    >
+                      Remember this device
+                    </label>
+                  </div>
+                  <a
+                    href="#"
+                    className="ml-auto text-sm font-normal text-black hover:underline"
+                  >
+                    Forgot Password?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full px-5 py-3 text-base font-medium text-center text-white tracking-widest bg-primary-blue rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
                 >
-                  Lost Password?
-                </a>
-              </div>
-              <button
-                type="submit"
-                className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
-              >
-                Login to your account
-              </button>
-              <div className="text-sm font-medium text-gray-900">
-                Not registered yet?{' '}
-                <a className="text-blue-600 hover:underline">Create account</a>
-              </div>
-            </form>
+                  Login
+                </button>
+                <div className="text-sm font-thin text-center text-gray-900">
+                  Not registered yet?{' '}
+                  <a className="font-medium text-black hover:underline">
+                    Create account
+                  </a>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
