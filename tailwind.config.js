86532db83fcc5d00cb1c7d8615b6515b4c16ffe5
kumuditha-udayanga <kumudithaudaya@gmail.com>
2023-11-18 16:39:06 +0530

    Implement login component design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ['./index.html', './src/**/*.{js,ts,jsx,tsx}'];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      'primary-blue': '#1677FF',
+    },
+  },
 };
 export const plugins = [];
