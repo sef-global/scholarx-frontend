fbac8978feee84ad5f4fdda60bc2d8afe64ea1f7
Kumuditha Karunarathna <kumudithaudaya@gmail.com>
2024-01-06 16:21:01 +0530

    Implement the admin dashboard UI (#67)

---
+++ src/components/Dashboard/scenes/Emails/Emails.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Emails: React.FC = () => {
+  return (
+    <div>
+      <h1>Emails</h1>
+    </div>
+  );
+};
+
+export default Emails;
---
+++ src/components/Dashboard/scenes/ManageMentorApplications/ManageMentorApplications.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageMentorApplications: React.FC = () => {
+  return (
+    <div>
+      <h1>Manage Mentor Applications</h1>
+    </div>
+  );
+};
+
+export default ManageMentorApplications;
