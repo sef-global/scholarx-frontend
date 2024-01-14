66bfb481338eee4799cd3495123095e5f60c6704
mayura-andrew <mayuraalahakoon@gmail.com>
2024-01-14 13:27:35 +0530

    Fixed LoginModal mobile responsiveness

--- src/components/LogoutModal/index.tsx
+++ src/components/LogoutModal/index.tsx
@@ -34,7 +34,7 @@ const LogoutModal: React.FC<LogoutModalProps> = ({ onClose }) => {
 
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto">
-      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div className="flex min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0 flex-col items-center justify-center h-screen w-full p-5 ">
         <div className="fixed inset-0 transition-opacity">
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
