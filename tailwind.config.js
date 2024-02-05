4acaddcbdc54d98cef754045914c8537e68a72b7
Hiran Ilangakoon <75526380+vishwaikon@users.noreply.github.com>
2024-02-05 20:39:01 +0530

    Develop Footer with Tailwind CSS (#79)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ export const theme = {
   extend: {
     colors: {
       'primary-blue': '#1677FF',
+      'darkmod-blue': '#3D317C',            //dark moderate blue
+      'desatdark-blue':'#6D7893',            //desaturated dark blue
     },
   },
 };
