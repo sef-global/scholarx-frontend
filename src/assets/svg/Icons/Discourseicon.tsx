6e83633ba64269073a309a47f6c3a9b3d5b6bf82
vishwaikon <hiranvishwailangakoon@gmail.com>
2024-02-05 16:21:34 +0530

    Develop Footer with Tailwind CSS

---
+++ src/assets/svg/Icons/Discourseicon.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Discourseicon: React.FC = () => {
+  return (
+    <div>
+      <svg
+        className="h-4"
+        fill="#ffffff"
+        viewBox="-2.4 -2.4 28.80 28.80"
+        xmlns="http://www.w3.org/2000/svg"
+        stroke="#ffffff"
+        strokeWidth="0.00024000000000000003"
+      >
+        <g
+          id="SVGRepo_bgCarrier"
+          strokeWidth="0"
+          transform="translate(5.64,5.64), scale(0.53)"
+        >
+          <rect
+            x="-2.4"
+            y="-2.4"
+            width="28.80"
+            height="28.80"
+            rx="0"
+            fill="#6271d5"
+            strokeWidth="0"
+          ></rect>
+        </g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          <path d="m12.102 0c-.024 0-.053 0-.081 0-6.559 0-11.891 5.252-12.02 11.779v.012c0 .209.006 12.209.006 12.209l12.097-.01c6.582-.055 11.897-5.404 11.897-11.995s-5.315-11.939-11.892-11.995h-.005zm-.102 18.857c-.005 0-.01 0-.015 0-1.053 0-2.05-.239-2.94-.666l.041.018-4.345 1.077 1.227-4.018c-.522-.945-.83-2.072-.83-3.27 0-3.788 3.071-6.859 6.859-6.859s6.859 3.071 6.859 6.859c0 3.787-3.069 6.858-6.856 6.859z"></path>
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default Discourseicon;
