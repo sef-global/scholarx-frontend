dc661874a72c264af5b78ecdcbad5fed6f9129e7
vishwaikon <hiranvishwailangakoon@gmail.com>
2024-02-05 12:21:29 +0530

    Added Suggestions

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ export const theme = {
   extend: {
     colors: {
       'primary-blue': '#1677FF',
-      'darkmod-blue': '#3D317C',
-      'desatdark-blue':'#6D7893',
+      'darkmod-blue': '#3D317C',            //dark moderate blue
+      'desatdark-blue':'#6D7893',            //desaturated dark blue
     },
   },
 };
