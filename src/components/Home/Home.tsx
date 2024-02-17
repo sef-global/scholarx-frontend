8ebd684444206a2b0ac668ccd418a68716fc90c8
vishwaikon <hiranvishwailangakoon@gmail.com>
2024-02-17 14:14:18 +0530

    Removed non essential CSS Classes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -153,7 +153,7 @@ const Home: React.FC = () => (
 
     {/* Success stories container */}
 
-    <div className="homePageContainernew p-[4%] bg-[#F8FDFF] flex  flex-col md:flex-row justify-center items-center">
+    <div className="homePageContainerNew p-[4%] bg-[#F8FDFF] flex  flex-col md:flex-row justify-center items-center">
       <div className="imageCol mb-2.5">
         <img
           className="max-w-[300px] mx-auto  w-[100%]"
@@ -162,11 +162,11 @@ const Home: React.FC = () => (
         />
       </div>
       <div className="textCol md:ml-16 ">
-        <div className="homepageSecondaryTitlenew font-sans text-3xl font-bold text-darkmod-blue text-center mb-2.5">
+        <div className="homepageSecondaryTitleNew font-sans text-3xl font-bold text-darkmod-blue text-center mb-2.5">
           Let`s hear it from our
           <br /> ScholarX Alumni
         </div>
-        <div className="homePageSecondaryButtonnew flex justify-center items-center">
+        <div className="homePageSecondaryButtonNew flex justify-center items-center">
           <a href="#">
             <button className=" shadow-[0_2px_0_rgba(5,145,255,0.1)] bg-[#ffffff] hover:bg-[#1677ff] text-[#FFFFFF] font-bold text-sm w-[214px] h-[34px] rounded mx-auto">
               ScholarX Success Stories
@@ -178,13 +178,13 @@ const Home: React.FC = () => (
 
     {/* Archive container */}
 
-    <div className="homePagecontainernew p-[4%] bg-[#F8FDFF] flex  flex-col md:flex-row justify-center items-center">
+    <div className="homePagecontainernew p-[4%] bg-[#F8FDFF] flex  flex-col md:flex-row justify-center items-center mb-1">
       <div className="textCol md:mr-16 ">
-        <div className="homepageSecondaryTitlenew font-sans text-3xl font-bold text-darkmod-blue text-center mb-2.5">
+        <div className="homepageSecondaryTitleNew font-sans text-3xl font-bold text-darkmod-blue text-center mb-2.5">
           Checkout our past
           <br /> mentors and mentees
         </div>
-        <div className="homePageSecondaryButtonnew flex justify-center items-center">
+        <div className="homePageSecondaryButtonNew flex justify-center items-center">
           <a href="#">
             <button className=" shadow-[0_2px_0_rgba(5,145,255,0.1)] bg-[#ffffff] hover:bg-[#1677ff] text-[#FFFFFF] font-bold text-sm w-[214px] h-[34px] rounded mx-auto">
               ScholarX Archive
@@ -192,7 +192,7 @@ const Home: React.FC = () => (
           </a>
         </div>
       </div>
-      <div className="imageCol mb-2.5">
+      <div className="imageCol ">
         <img
           className="max-w-[360px] mx-auto  w-[100%]"
           src="../../../public/scholarX-archive.png"
