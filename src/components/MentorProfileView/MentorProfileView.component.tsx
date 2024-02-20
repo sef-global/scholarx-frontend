d9f97a839bdf7d02a24a991f878c7742ea8810a7
Randila Premarathne <randilamenukapremarathne@gmail.com>
2024-02-20 14:16:55 +0530

    Add past mentees part

--- src/components/MentorProfileView/MentorProfileView.component.tsx
+++ src/components/MentorProfileView/MentorProfileView.component.tsx
@@ -67,12 +67,89 @@ const MentorProfileView: React.FC = () => {
           aliquip ex ea commodo consequat. Duis aute irure dolor in
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.{' '}
+          culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
       <div>
-        <h1 className="text-xl font-extrabold">Success Stories</h1>
+        <h1 className="text-xl font-extrabold mb-2">Success Stories</h1>
         <hr />
+        <div className="h-1/3 flex flex-row gap-4 my-9 ">
+          <div className="w-1/6  h-64 border-2"></div>
+          <div className="w-1/6  h-64 border-2"></div>
+          <div className="w-1/6  h-64 border-2"></div>
+          <div className="w-1/6  h-64 border-2"></div>
+          <div className="w-1/6  h-64 border-2"></div>
+          <div className="w-1/6  h-64 border-2"></div>
+        </div>
+      </div>
+      <div>
+        <h1 className="text-xl font-extrabold">Past Mentees</h1>
+        <hr />
+        <div className="mt-9">
+          <div className="flex flex-row gap-4">
+            <div className="relative w-80">
+              <input
+                type="text"
+                id="search"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Type the keywords here to search"
+                required
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 end-0 flex items-center pe-3"
+              >
+                <svg
+                  className="w-4 h-4 me-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </button>
+            </div>
+            <button>Advanced options</button>
+          </div>
+          <div className="w-full mt-9">
+            <div className="w-full h-16 border-gray-200 border-2 grid grid-cols-6 mb-4">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Andrew_Tate_on_%27Anything_Goes_With_James_English%27_in_2021.jpg"
+                className="h-10 w-10 object-fill rounded-full my-auto mx-auto"
+                alt="past mentee"
+              />
+              <h3 className="h-5 my-auto text-base font-bold">John Doe</h3>
+              <p className="h-5 my-auto text-sm">University of Colombo</p>
+            </div>
+
+            <div className="w-full h-16 border-gray-200 border-2 grid grid-cols-6 mb-4">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Andrew_Tate_on_%27Anything_Goes_With_James_English%27_in_2021.jpg"
+                className="h-10 w-10 object-fill rounded-full my-auto mx-auto"
+                alt="past mentee"
+              />
+              <h3 className="h-5 my-auto text-base font-bold">John Doe</h3>
+              <p className="h-5 my-auto text-sm">University of Colombo</p>
+            </div>
+
+            <div className="w-full h-16 border-gray-200 border-2 grid grid-cols-6 mb-4">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Andrew_Tate_on_%27Anything_Goes_With_James_English%27_in_2021.jpg"
+                className="h-10 w-10 object-fill rounded-full my-auto mx-auto"
+                alt="past mentee"
+              />
+              <h3 className="h-5 my-auto text-base font-bold">John Doe</h3>
+              <p className="h-5 my-auto text-sm">University of Colombo</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
