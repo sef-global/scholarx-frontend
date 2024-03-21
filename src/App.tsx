2bc65f7c0b75dbe775db90bd8f19fcafbd8a5587
anjula-sack <anjulashanaka@gmail.com>
2024-03-21 22:40:08 +0530

    Add RHF

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
 import React, { useContext, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './components/Home/Home';
 import MainLayout from './components/Layout/MainLayout';
 import { UserContext, type UserContextType } from './contexts/UserContext';
 import MentorRegistrationPage from './components/MentorRegistrationPage';
 import Dashboard from './components/Dashboard/Dashboard.tsx';
 
+const queryClient = new QueryClient();
+
 const App: React.FC = () => {
-  const { user, getUser } = useContext(UserContext) as UserContextType;
+  const { getUser } = useContext(UserContext) as UserContextType;
 
   useEffect(() => {
     getUser();
   }, []);
 
-  user !== null &&
-    console.log(`user is authenticated as ${user.primary_email}`);
-
   return (
-    <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/mentor-registration"
-            element={<MentorRegistrationPage />}
-          />
-          <Route path="/dashboard/*" element={<Dashboard />} />
-        </Routes>
-      </MainLayout>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/mentor-registration"
+              element={<MentorRegistrationPage />}
+            />
+            <Route path="/dashboard/*" element={<Dashboard />} />
+          </Routes>
+        </MainLayout>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
--- src/components/MentorRegistrationPage/index.tsx
+++ src/components/MentorRegistrationPage/index.tsx
@@ -1,98 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { API_URL } from '../../constants';
+import useCategories from '../../hooks/useCategories';
+import type { MentorApplication } from '../../types';
 
 const MentorRegistrationPage: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<MentorApplication>({ defaultValues: { isPastMentor: false } });
+  const { error, data: categories } = useCategories();
   const [currentStep, setCurrentStep] = useState(0);
-  const [error, setError] = useState<string | null>(null);
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    linkedinUrl: '',
-    researchGateUrl: '',
-    googleScholarUrl: '',
-    categoryId: '',
-    country: '',
-    expertise: '',
-    mentoringStrategy: '',
-  });
-  const [categories, setCategories] = useState([]);
 
   const handleNext = (): void => {
     setCurrentStep((prevStep) => prevStep + 1);
   };
 
-  const handleInputChange = (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-    >
-  ): void => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
+  const onSubmit: SubmitHandler<MentorApplication> = (data): void => {
+    console.log(data);
 
-  const handleSubmit = (): void => {
-    setError(null);
-    axios
-      .post(
-        `${API_URL}/mentors`,
-        {
-          application: [
-            { question: 'First Name', answer: formData.firstName },
-            { question: 'Last Name', answer: formData.lastName },
-            { question: 'Linkedin Url', answer: formData.linkedinUrl },
-            { question: 'ResearchGate Url', answer: formData.researchGateUrl },
-            {
-              question: 'GoogleScholar Url',
-              answer: formData.googleScholarUrl,
-            },
-            { question: 'Country', answer: formData.country },
-            { question: 'What is your expertise', answer: formData.expertise },
-            {
-              question: 'What is your mentoring Strategy',
-              answer: formData.mentoringStrategy,
-            },
-          ],
-          categoryId: formData.categoryId,
-        },
-        { withCredentials: true }
-      )
-      .then(() => {
-        window.location.href = '/';
-      })
-      .catch((error) => {
-        if (error.response.status !== 401) {
-          setError(error.response.data.message);
-          console.error({
-            message:
-              'Something went wrong with submitting the mentor application',
-            description: error,
-          });
-        } else {
-          console.error('Error submitting mentor application:', error);
-        }
-      });
+    // axios
+    //   .post(
+    //     `${API_URL}/mentors`,
+    //     {
+    //       application: [
+    //       ],
+    //       categoryId: formData.categoryId,
+    //     },
+    //     { withCredentials: true }
+    //   )
+    //   .then(() => {
+    //     window.location.href = '/';
+    //   })
+    //   .catch((error) => {
+    //     if (error.response.status !== 401) {
+    //       console.error({
+    //         message:
+    //           'Something went wrong with submitting the mentor application',
+    //         description: error,
+    //       });
+    //     } else {
+    //       console.error('Error submitting mentor application:', error);
+    //     }
+    //   });
   };
-
-  useEffect(() => {
-    axios
-      .get(`${API_URL}/categories`)
-      .then((response) => {
-        const categories = response.data.categories;
-        setFormData((prevData) => ({
-          ...prevData,
-          categoryId: categories[0].uuid,
-        }));
-        setCategories(categories);
-      })
-      .catch((error) => {
-        console.error('Error fetching categories:', error);
-      });
-  }, []);
 
   return (
     <div className="min-h-screen flex justify-center items-start">
-      <div className="relative max-w-lg w-full p-8">
+      <div className="relative w-1/2 p-8">
         <div className="text-2xl font-semibold mb-2">Become a Mentor</div>
         <hr className="border-t border-gray-300 mb-6" />
         <div className="relative h-2 mb-10">
@@ -104,88 +62,63 @@ const MentorRegistrationPage: React.FC = () => {
             <div className="pt-3">Step {currentStep + 1} of 3</div>
           </div>
         </div>
-        <div className="space-y-4" onSubmit={handleSubmit}>
+        <div className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
           {currentStep === 0 && (
             <>
+              <div className="text-xl font-medium mb-2">
+                Personal Information
+              </div>
+              <hr />
               <div className="flex flex-wrap">
                 <div className="w-full md:w-1/2 md:pr-1 mb-4 md:mb-0">
-                  <label
-                    htmlFor="firstName"
-                    className="block text-sm font-medium text-gray-600"
-                  >
+                  <label className="block text-sm font-medium text-gray-600">
                     First Name
                   </label>
                   <input
                     type="text"
-                    id="firstName"
-                    name="firstName"
-                    value={formData.firstName}
-                    onChange={handleInputChange}
+                    {...register('firstName')}
                     className="mt-1 p-2 w-full border rounded-md"
                   />
                 </div>
                 <div className="w-full md:w-1/2 md:pl-1">
-                  <label
-                    htmlFor="lastName"
-                    className="block text-sm font-medium text-gray-600"
-                  >
+                  <label className="block text-sm font-medium text-gray-600">
                     Last Name
                   </label>
                   <input
                     type="text"
-                    id="lastName"
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleInputChange}
+                    {...register('lastName')}
                     className="mt-1 p-2 w-full border rounded-md"
                   />
                 </div>
               </div>
               <div className="mb-4">
-                <label
-                  htmlFor="linkedinUrl"
-                  className="block text-sm font-medium text-gray-600"
-                >
-                  LinkedIn URL
+                <label className="block text-sm font-medium text-gray-600">
+                  Email
                 </label>
                 <input
                   type="text"
-                  id="linkedinUrl"
-                  name="linkedinUrl"
-                  value={formData.linkedinUrl}
-                  onChange={handleInputChange}
+                  {...register('email')}
                   className="mt-1 p-2 w-full border rounded-md"
                 />
               </div>
               <div className="mb-4">
-                <label
-                  htmlFor="researchGateUrl"
-                  className="block text-sm font-medium text-gray-600"
-                >
-                  ResearchGate URL
+                <label className="block text-sm font-medium text-gray-600">
+                  Contact No (Whatsapp)
                 </label>
                 <input
                   type="text"
-                  id="researchGateUrl"
-                  name="researchGateUrl"
-                  value={formData.researchGateUrl}
-                  onChange={handleInputChange}
+                  {...register('contactNo')}
                   className="mt-1 p-2 w-full border rounded-md"
                 />
               </div>
+
               <div className="mb-4">
-                <label
-                  htmlFor="googleScholarUrl"
-                  className="block text-sm font-medium text-gray-600"
-                >
-                  GoogleScholar URL
+                <label className="block text-sm font-medium text-gray-600">
+                  Country
                 </label>
                 <input
                   type="text"
-                  id="googleScholarUrl"
-                  name="googleScholarUrl"
-                  value={formData.googleScholarUrl}
-                  onChange={handleInputChange}
+                  {...register('country')}
                   className="mt-1 p-2 w-full border rounded-md"
                 />
               </div>
@@ -193,19 +126,36 @@ const MentorRegistrationPage: React.FC = () => {
           )}
           {currentStep === 1 && (
             <>
+              <div className="text-xl font-medium mb-2">
+                Professional Information
+              </div>
               <div className="mb-4">
-                <label
-                  htmlFor="categoryId"
-                  className="block text-sm font-medium text-gray-600"
-                >
+                <label className="block text-sm font-medium text-gray-600">
+                  Institution
+                </label>
+                <input
+                  type="text"
+                  {...register('institution')}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
+                  Current Position
+                </label>
+                <input
+                  type="text"
+                  {...register('position')}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
                   Category
                 </label>
                 <select
                   className="mt-1 p-2 w-1/2 border rounded-md"
-                  name="categoryId"
-                  id="categoryId"
-                  value={formData.categoryId}
-                  onChange={handleInputChange}
+                  {...register('category')}
                 >
                   {categories.map(
                     (category: { uuid: string; category: string }) => (
@@ -217,35 +167,51 @@ const MentorRegistrationPage: React.FC = () => {
                 </select>
               </div>
               <div className="mb-4">
-                <label
-                  htmlFor="country"
-                  className="block text-sm font-medium text-gray-600"
-                >
-                  What is your country?
+                <label className="block text-sm font-medium text-gray-600">
+                  Areas of Expertise
                 </label>
                 <input
                   type="text"
-                  id="country"
-                  name="country"
-                  value={formData.country}
-                  onChange={handleInputChange}
+                  {...register('expertise')}
                   className="mt-1 p-2 w-full border rounded-md"
                 />
               </div>
-
               <div className="mb-4">
-                <label
-                  htmlFor="expertise"
-                  className="block text-sm font-medium text-gray-600"
-                >
-                  What is your expertise?
+                <label className="block text-sm font-medium text-gray-600">
+                  Brief Bio (max. 200 characters)
+                </label>
+                <textarea
+                  {...register('bio')}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
+                  Linkedin
                 </label>
                 <input
                   type="text"
-                  id="expertise"
-                  name="expertise"
-                  value={formData.expertise}
-                  onChange={handleInputChange}
+                  {...register('linkedin')}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
+                  Website
+                </label>
+                <input
+                  type="text"
+                  {...register('website')}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
+                  CV
+                </label>
+                <input
+                  type="text"
+                  {...register('cv')}
                   className="mt-1 p-2 w-full border rounded-md"
                 />
               </div>
@@ -253,18 +219,102 @@ const MentorRegistrationPage: React.FC = () => {
           )}
           {currentStep === 2 && (
             <>
+              <div className="text-xl font-medium mb-2">Mentorship Details</div>
               <div className="mb-4">
-                <label
-                  htmlFor="mentoringStrategy"
-                  className="block text-sm font-medium text-gray-600"
-                >
-                  What is your mentoring strategy?
+                <label className="block text-sm font-medium text-gray-600">
+                  Are you a past mentor?
+                </label>
+                <input
+                  type="checkbox"
+                  {...register('isPastMentor')}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              {watch('isPastMentor') && (
+                <>
+                  <div className="mb-4">
+                    <label className="block text-sm font-medium text-gray-600">
+                      Which year?
+                    </label>
+                    <input
+                      type="number"
+                      {...register('mentoredYear')}
+                      className="mt-1 p-2 w-full border rounded-md"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block text-sm font-medium text-gray-600">
+                      What was your motivation for joining the program? Has it
+                      changed, if yes, how?
+                    </label>
+                    <input
+                      type="text"
+                      {...register('motivation')}
+                      className="mt-1 p-2 w-full border rounded-md"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block text-sm font-medium text-gray-600">
+                      Why would like to be a ScholarX mentor
+                    </label>
+                    <input
+                      type="text"
+                      {...register('reasonToMentor')}
+                      className="mt-1 p-2 w-full border rounded-md"
+                    />
+                  </div>
+                </>
+              )}
+
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
+                  Reason to Mentor
+                </label>
+                <input
+                  type="text"
+                  {...register('reasonToMentor')}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
+                  Mentee Expectations (short description: who is your ideal
+                  mentee?)
+                </label>
+                <input
+                  type="text"
+                  {...register('menteeExpectations')}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
+                  What is your mentoring philosophy?
                 </label>
                 <textarea
-                  id="mentoringStrategy"
-                  name="mentoringStrategy"
-                  value={formData.mentoringStrategy}
-                  onChange={handleInputChange}
+                  {...register('mentoringPhilosophy')}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
+                  How many mentees can you accommodate?
+                </label>
+                <input
+                  type="number"
+                  {...register('noOfMentees')}
+                  className="mt-1 p-2 border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
+                  Are you able to commit to a period of 6 months for the
+                  program? (Support description: We expect a minimum of 6 calls
+                  with a mentee in a span of 6 month period)
+                </label>
+                <input
+                  type="checkbox"
+                  {...register('canCommit')}
                   className="mt-1 p-2 w-full border rounded-md"
                 />
               </div>
@@ -275,7 +325,7 @@ const MentorRegistrationPage: React.FC = () => {
               className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:text-red-400"
               role="alert"
             >
-              {error}
+              {error.message}
             </div>
           ) : null}
           <hr className="border-t border-gray-300 my-6" />
@@ -301,8 +351,7 @@ const MentorRegistrationPage: React.FC = () => {
               </button>
             ) : (
               <button
-                type="button"
-                onClick={handleSubmit}
+                type="submit"
                 className="text-white bg-blue-500 hover:bg-blue-600 focus:ring-1 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-small rounded-md text-sm inline-flex items-center px-3 py-1.5 text-center me-2"
               >
                 Submit
