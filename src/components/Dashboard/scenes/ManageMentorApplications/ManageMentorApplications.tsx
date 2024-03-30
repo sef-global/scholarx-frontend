27296a0d59e80a703518c6c4e9f1e43e56ebb25e
Madhawa Monarawila <madawammonarawila@gmail.com>
2024-03-31 00:49:53 +0530

    Update ManageMentorApplications styling

--- src/components/Dashboard/scenes/ManageMentorApplications/ManageMentorApplications.tsx
+++ src/components/Dashboard/scenes/ManageMentorApplications/ManageMentorApplications.tsx
@@ -41,7 +41,7 @@ export const ManageMentorApplications: React.FC = () => {
     return (
       <>
         {mentors !== undefined && (
-          <div className="flex mb-4">
+          <div className="flex mb-3">
             {filters.map(({ label, status }) => {
               const count = mentors.filter((mentor) =>
                 status.length > 0 ? mentor.state === status : true
@@ -49,7 +49,7 @@ export const ManageMentorApplications: React.FC = () => {
               return (
                 <button
                   key={label}
-                  className={`px-4 py-2 font-medium text-sm ${
+                  className={`px-4 py-1 font-medium  ${
                     filter === status
                       ? 'bg-blue-500 text-white'
                       : 'bg-gray-200 text-gray-700 hover:bg-gray-300'
@@ -122,16 +122,16 @@ export const ManageMentorApplications: React.FC = () => {
         <table className="w-full">
           <thead>
             <tr>
-              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 tracking-wider">
+              <th className="px-6 py-2 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 tracking-wider  w-1/4">
                 Name
               </th>
-              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 tracking-wider">
+              <th className="px-6 py-2 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 tracking-wider  w-1/4">
                 Profession
               </th>
-              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 tracking-wider">
+              <th className="px-6 py-2 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 tracking-wider  w-1/4">
                 Category
               </th>
-              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 tracking-wider">
+              <th className="px-6 py-2 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 tracking-wider  w-1/4">
                 Status
               </th>
             </tr>
@@ -139,22 +139,22 @@ export const ManageMentorApplications: React.FC = () => {
           <tbody className="bg-white">
             {filteredMentorsByName.map((mentor) => (
               <tr key={mentor.uuid}>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                <td className="px-6 py-2 whitespace-no-wrap border-b border-gray-200 w-1/4">
                   {mentor.application.firstName} {mentor.application.lastName}
                 </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                <td className="px-6 py-2 whitespace-no-wrap border-b border-gray-200 w-1/4">
                   {mentor.application.position}
                 </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                <td className="px-6 py-2 whitespace-no-wrap border-b border-gray-200 w-1/4">
                   {mentor.category.category}
                 </td>
-                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                <td className="py-2 whitespace-no-wrap border-b border-gray-200 w-1/4">
                   <select
                     value={mentor.state}
                     onChange={async (e) => {
                       await handleStatusUpdate(mentor.uuid, e.target.value);
                     }}
-                    className="py-2 px-4 border border-gray-300 rounded-md"
+                    className="py-1.5 px-5 border border-gray-300 rounded-md"
                   >
                     <option value="pending">Pending</option>
                     <option value="rejected">Rejected</option>
@@ -172,30 +172,27 @@ export const ManageMentorApplications: React.FC = () => {
   return (
     <div className="container mx-auto p-4 bg-white min-h-full min-w-full">
       <h1 className="text-2xl font-medium my-4">Manage Mentor Applications</h1>
-      <hr className="my-6" />
+      <hr className="my-4" />
       {renderFilters()}
       {isLoading ? (
         <div className="flex justify-center items-center h-64">
           <svg
-            className="animate-spin h-10 w-10 text-blue-500"
-            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            className="inline w-8 h-8 text-gray-200 animate-spin fill-blue-600"
+            viewBox="0 0 100 101"
             fill="none"
-            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
             <path
-              className="opacity-75"
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
               fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
           </svg>
+          <span className="sr-only">Loading...</span>
         </div>
       ) : (
         renderMentorTable()
