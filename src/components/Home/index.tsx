a565017e41f899f3175a6103d6917f34335783c6
mayura-andrew <mayuraalahakoon@gmail.com>
2024-06-02 11:59:29 +0530

    Change Apply button text to Find a Mentor

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
             <div className="flex mt-10">
               <Link to="/mentors">
                 <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
-                  Apply
+                  Find a Mentor
                 </button>
               </Link>
             </div>
