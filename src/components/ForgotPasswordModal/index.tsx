8146fc2ce7026b3ff65f2065806b32f3f32dd124
mayura-andrew <mayuraalahakoon@gmail.com>
2024-06-08 11:36:57 +0530

    Add react-hook-form for form validation in ForgotPasswordModal

--- src/components/ForgotPasswordModal/index.tsx
+++ src/components/ForgotPasswordModal/index.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useResetPassword } from '../../hooks/useForgotPassword';
 import { type PasswordResetData } from '../../types';
 import { z } from 'zod';
 import Loading from '../../assets/svg/Loading';
 import closeIcon from '../../assets/svg/closeIcon.svg';
-import { useNavigate } from 'react-router-dom';
 
 const ForgotPasswordDataSchema = z.object({
   email: z.string(),
@@ -17,40 +17,31 @@ interface ForgotPasswordModalProps {
 const ForgotPasswordModal: React.FC<ForgotPasswordModalProps> = ({
   handleClose,
 }) => {
-  const [email, setEmail] = useState('');
+  const { register, handleSubmit } = useForm<PasswordResetData>();
+
   const [message, setMessage] = useState('');
   const { requestPasswordReset } = useResetPassword();
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-  const [formData, setFormData] = useState<PasswordResetData>({
-    email: '',
-  });
 
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onSubmit = (data: PasswordResetData) => {
     setLoading(true);
 
-    const validatedData = ForgotPasswordDataSchema.parse(formData);
+    const validatedData = ForgotPasswordDataSchema.parse(data);
     requestPasswordReset(validatedData, {
       onSuccess: () => {
         setMessage(
-          `A password reset link has been sent to ${email}. Please check your email and follow the instructions to reset your password. You may now close this dialog if you wish.`
+          `A password reset link has been sent to your email address. Please check your email and follow the instructions to reset your password. You may now close this dialog if you wish.`
         );
         setLoading(false);
         setTimeout(() => {
-          navigate('/');
-        }, 500);
+          handleClose();
+        }, 1000);
       },
       onError: () => {
         setMessage('Error sending email, Check your email and try again.');
         setLoading(false);
       },
     });
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-    setFormData({ email: e.target.value });
   };
 
   return (
@@ -78,14 +69,12 @@ const ForgotPasswordModal: React.FC<ForgotPasswordModalProps> = ({
               <h2 className="text-2xl mb-5 font-semibold text-gray-900 text-center">
                 Forgot Your Password ?
               </h2>
-              <form onSubmit={handleFormSubmit}>
+              <form onSubmit={handleSubmit(onSubmit)}>
                 <input
                   type="email"
-                  name="email"
+                  {...register('email')}
                   placeholder="Enter your email address"
-                  value={email}
                   className="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                  onChange={handleInputChange}
                   required
                 />
                 <div>
@@ -96,12 +85,14 @@ const ForgotPasswordModal: React.FC<ForgotPasswordModalProps> = ({
                       </>
                     </div>
                   ) : (
-                    <button
-                      type="submit"
-                      className="w-full px-4 py-2 text-base font-semibold text-center text-white bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 cursor-pointer"
-                    >
-                      Submit
-                    </button>
+                    <div className="flex justify-center items-center">
+                      <button
+                        type="submit"
+                        className="w-full px-4 py-2 text-base font-semibold text-center text-white bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 cursor-pointer shadow-md transition-all md:w-auto"
+                      >
+                        Submit
+                      </button>
+                    </div>
                   )}
                   {message.length > 0 && (
                     <div className=" text-green-500 px-4 py-3 my-3">
