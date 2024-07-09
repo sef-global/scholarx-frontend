7f55db374ab8da3638cfa269bb0fb252a49bc00c
anjula-sack <anjulashanaka@gmail.com>
2024-07-09 21:16:23 +0530

    Add previous mentors

--- src/components/MentorCard/MentorCard.component.tsx
+++ src/components/MentorCard/MentorCard.component.tsx
@@ -10,22 +10,30 @@ interface MentorCardProps {
 const MentorCard: React.FC<MentorCardProps> = ({ mentor }) => {
   return (
     <Link
-      className="border border-gray-200 p-4 rounded-md shadow-md w-52 flex flex-col h-full"
+      className="border border-gray-200 p-4 rounded-md shadow-md w-52 flex flex-col h-full relative"
       to={`/mentors/${mentor.uuid}`}
     >
       {mentor.profile.image_url !== '' ? (
         <img
           src={mentor.profile.image_url}
           alt="Mentor Avatar"
-          className="w-24 h-24 rounded-full mx-auto mb-4 object-cover"
+          className={`w-24 h-24 rounded-full mx-auto my-4 object-cover ${
+            !mentor.availability && 'opacity-60'
+          }`}
+          referrerPolicy="no-referrer"
         />
       ) : (
-        <div className="w-24 h-24 bg-gray-200 rounded-full mx-auto mb-4 flex items-center justify-center">
+        <div className="w-24 h-24 bg-gray-200 rounded-full mx-auto my-4 flex items-center justify-center">
           <UserIcon />
         </div>
       )}
+      {!mentor.availability && (
+        <div className="absolute top-0 left-0 bg-white bg-opacity-75 text-gray-600 px-2 py-1 rounded-md">
+          <span className="text-xs font-semibold">Unavailable</span>
+        </div>
+      )}
       <div className="text-center">
-        <h5 className="text-lg font-semibold">
+        <h5 className="text-lg font-semibold text-slate-600 text-wrap">
           {mentor.application.firstName} {mentor.application.lastName}
         </h5>
         <p className="text-sm">{mentor.application.position}</p>
