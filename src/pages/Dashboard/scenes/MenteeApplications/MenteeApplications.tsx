6afa7c6efe43d39d4d08e3497fea75d1e2ba4fca
Madhawa Monarawila <70215958+Madhawa97@users.noreply.github.com>
2024-09-09 20:24:15 +0530

    Add pagination support for frontend (#182)

--- src/pages/Dashboard/scenes/MenteeApplications/MenteeApplications.tsx
+++ src/pages/Dashboard/scenes/MenteeApplications/MenteeApplications.tsx
@@ -1,30 +1,88 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { type Mentee } from '../../../../types';
 import useMentees from '../../../../hooks/admin/useMentees';
 import { Link } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
+import Loading from '../../../../assets/svg/Loading';
 
 const MenteeApplications: React.FC = () => {
   const [filter, setFilter] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
-  const { isLoading, data: mentees } = useMentees();
+  const pageSize = 10;
+
+  const { ref, inView } = useInView();
+
+  const {
+    data: mentees,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status: menteesStatus,
+  } = useMentees(filter, pageSize);
+  const { status: allMenteesStatus, totalItemCount: totalAllItemCount } =
+    useMentees(null, 1);
+  const {
+    status: approvedMenteesStatus,
+    totalItemCount: totalApprovedItemCount,
+  } = useMentees('approved', 1);
+  const {
+    status: pendingMenteesStatus,
+    totalItemCount: totalPendingItemCount,
+  } = useMentees('pending', 1);
+  const {
+    status: rejectedMenteesStatus,
+    totalItemCount: totalRejectedItemCount,
+  } = useMentees('rejected', 1);
+  const {
+    status: completedMenteesStatus,
+    totalItemCount: totalCompletedItemCount,
+  } = useMentees('completed', 1);
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      void fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage]);
 
   const renderFilters = () => {
     const filters = [
-      { label: 'All', status: '' },
-      { label: 'Approved', status: 'approved' },
-      { label: 'Pending', status: 'pending' },
-      { label: 'Rejected', status: 'rejected' },
-      { label: 'Completed', status: 'completed' },
+      {
+        label: 'All',
+        status: '',
+        count: totalAllItemCount,
+        isLoaded: allMenteesStatus === 'success',
+      },
+      {
+        label: 'Approved',
+        status: 'approved',
+        count: totalApprovedItemCount,
+        isLoaded: approvedMenteesStatus === 'success',
+      },
+      {
+        label: 'Pending',
+        status: 'pending',
+        count: totalPendingItemCount,
+        isLoaded: pendingMenteesStatus === 'success',
+      },
+      {
+        label: 'Rejected',
+        status: 'rejected',
+        count: totalRejectedItemCount,
+        isLoaded: rejectedMenteesStatus === 'success',
+      },
+      {
+        label: 'Completed',
+        status: 'completed',
+        count: totalCompletedItemCount,
+        isLoaded: completedMenteesStatus === 'success',
+      },
     ];
 
     return (
       <>
         {mentees !== undefined && (
           <div className="flex mb-3">
-            {filters.map(({ label, status }) => {
-              const count = mentees?.filter((mentee) =>
-                status.length > 0 ? mentee.state === status : true
-              ).length;
+            {filters.map(({ label, status, count, isLoaded }) => {
               return (
                 <button
                   key={label}
@@ -43,7 +101,7 @@ const MenteeApplications: React.FC = () => {
                     setFilter(status);
                   }}
                 >
-                  {label} ({count})
+                  {label} {isLoaded && `(${count})`}
                 </button>
               );
             })}
@@ -54,14 +112,14 @@ const MenteeApplications: React.FC = () => {
   };
 
   const renderMentorTable = () => {
-    const filteredMentors =
+    const filteredMentees =
       mentees != null
         ? filter !== ''
           ? mentees.filter((mentee) => mentee.state === filter)
           : mentees
         : [];
 
-    const filteredMentorsByName = filteredMentors.filter((mentor: Mentee) =>
+    const filteredMenteesByName = filteredMentees.filter((mentor: Mentee) =>
       `${mentor.application.firstName} ${mentor.application.lastName}`
         .toLowerCase()
         .includes(searchTerm.toLowerCase())
@@ -97,7 +155,7 @@ const MenteeApplications: React.FC = () => {
             </tr>
           </thead>
           <tbody className="bg-white">
-            {filteredMentorsByName.map((mentee) => (
+            {filteredMenteesByName.map((mentee) => (
               <tr key={mentee.uuid}>
                 <td className="px-6 py-2 whitespace-no-wrap border-b border-gray-200 w-1/4 text-blue-500">
                   <Link
@@ -125,6 +183,16 @@ const MenteeApplications: React.FC = () => {
                 </td>
               </tr>
             ))}
+
+            {isFetchingNextPage && (
+              <tr>
+                <td colSpan={3} className="text-center py-4">
+                  <Loading />
+                </td>
+              </tr>
+            )}
+
+            <tr ref={ref} style={{ height: '20px' }} />
           </tbody>
         </table>
       </>
@@ -136,24 +204,9 @@ const MenteeApplications: React.FC = () => {
       <h1 className="text-2xl font-medium my-4">Manage Mentee Applications</h1>
       <hr className="my-4" />
       {renderFilters()}
-      {isLoading ? (
+      {menteesStatus === 'pending' ? (
         <div className="flex justify-center items-center h-64">
-          <svg
-            aria-hidden="true"
-            className="inline w-8 h-8 text-gray-200 animate-spin fill-blue-600"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
+          <Loading />
         </div>
       ) : (
         renderMentorTable()
