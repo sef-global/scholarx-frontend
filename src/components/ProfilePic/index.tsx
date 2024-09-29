dc66eca5bd1326f17c429b3e1ebb4dc9386617b0
Ansak Mahir <ansarmmm.21@uom.lk>
2024-09-29 13:17:27 +0530

    lint fix

--- src/components/ProfilePic/index.tsx
+++ src/components/ProfilePic/index.tsx
@@ -7,8 +7,8 @@ const ProfilePic: React.FC<{
   size: string;
   width?: string;
   availability?: boolean;
-  circular?: boolean; 
-}> = ({ src, alt, size, width, availability = true, circular = true }) => { 
+  circular?: boolean;
+}> = ({ src, alt, size, width, availability = true, circular = true }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
@@ -22,7 +22,7 @@ const ProfilePic: React.FC<{
   };
 
   const appliedWidth = width || size;
-  
+
   return (
     <div
       className="relative inline-block"
@@ -30,13 +30,17 @@ const ProfilePic: React.FC<{
     >
       {isLoading && !isError && (
         <div
-          className={`absolute inset-0 bg-gray-200 animate-skeleton bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 bg-[length:200%_100%] ${circular ? 'rounded-full' : 'rounded-custom-5'}`} // Conditional rounded class
+          className={`absolute inset-0 bg-gray-200 animate-skeleton bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 bg-[length:200%_100%] ${
+            circular ? 'rounded-full' : 'rounded-custom-5'
+          }`} // Conditional rounded class
           style={{ height: size, width: appliedWidth }}
         />
       )}
       {isError ? (
         <div
-          className={`inline-block bg-gray-200 ring-2 ring-white flex items-center justify-center ${circular ? 'rounded-full' : 'rounded-custom-5'}`} // Conditional rounded class
+          className={`inline-block bg-gray-200 ring-2 ring-white flex items-center justify-center ${
+            circular ? 'rounded-full' : 'rounded-custom-5'
+          }`} // Conditional rounded class
           style={{ height: size, width: appliedWidth }}
         >
           <UserIcon />
@@ -45,14 +49,13 @@ const ProfilePic: React.FC<{
         <img
           src={src}
           alt={alt}
-          className={`inline-block ring-2 ring-white object-cover ${circular ? 'rounded-full' : 'rounded-custom-5'} ${
-            !availability ? 'opacity-60' : ''
-          } max-w-full`} 
+          className={`inline-block ring-2 ring-white object-cover ${
+            circular ? 'rounded-full' : 'rounded-custom-5'
+          } ${!availability ? 'opacity-60' : ''} max-w-full`}
           style={{ height: size, width: appliedWidth }}
           referrerPolicy="no-referrer"
           onLoad={handleImageLoad}
           onError={handleImageError}
-          
         />
       )}
     </div>
